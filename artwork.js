4f2ebcd939cdb0020618956fb6a33c89d80940ed
Christina Brasco <christina.brasco@gmail.com>
2019-05-20 21:04:36 -0700

    Adding artwork INSERT functionality

--- artwork.js
+++ artwork.js
@@ -5,7 +5,7 @@ module.exports = function() {
 
 	function getGenes(res, mysql, context, complete)
 	{
-		mysql.pool.query("SELECT name FROM gene", function(error, results, fields){
+		mysql.pool.query("SELECT name, id FROM gene", function(error, results, fields){
 			if (error)
 			{
 				res.write(JSON.stringify(error));
@@ -19,7 +19,7 @@ module.exports = function() {
 
 	function getArtists(res, mysql, context, complete)
 	{
-		mysql.pool.query("SELECT name FROM artist", function(error, results, fields){
+		mysql.pool.query("SELECT name, id FROM artist", function(error, results, fields){
 			if (error)
 			{
 				res.write(JSON.stringify(error));
@@ -32,7 +32,7 @@ module.exports = function() {
 
 	function getPartners(res, mysql, context, complete)
 	{
-		mysql.pool.query("SELECT name FROM partner", function(error, results, fields){
+		mysql.pool.query("SELECT name, id FROM partner", function(error, results, fields){
 			if (error)
 			{
 				res.write(JSON.stringify(error));
@@ -81,6 +81,34 @@ module.exports = function() {
 			if (callbackCount>=4)
 				res.render('artwork',context);
 		}
+	});
+
+
+
+
+	router.post("/", function(req,res){
+		var mysql = req.app.get('mysql');
+		var sql = "INSERT INTO artwork (title, artist_id, category, date, thumbnail_url, partner_id) VALUES (?, ?, ?, ?, ?, ?);";
+		var inserts = [req.body.artwork_title, req.body.artist_id, req.body.artwork_category, req.body.artwork_date, req.body.artwork_thumbnail,req.body.artwork_partner];
+		sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+			if(error) {
+				res.write(JSON.stringify(error));
+				res.end;
+			}
+			else {
+			sql = "INSERT INTO artwork_gene (artwork_id, gene_id) VALUES ((SELECT id FROM artwork where title=? and artist_id = ? and date = ?), ?);";
+			inserts = [req.body.artwork_title, req.body.artist_id, req.body.artwork_date, parseInt(req.body.genes_to_link)];
+			sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+				if(error) {
+					res.write(JSON.stringify(error));
+					res.end;
+				}
+				else {
+					res.redirect('/artist');
+				}
+			});
+			}
+		});			
 	});
 
 	return router;
