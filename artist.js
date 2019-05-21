0d864bce9fecdc75c44f73771bb3adabf250785c
Christina Brasco <christina.brasco@gmail.com>
2019-05-20 20:56:19 -0700

    splitting up queries

--- artist.js
+++ artist.js
@@ -63,21 +63,37 @@ module.exports = function() {
 	router.post("/", function(req,res){
 		console.log(req.body);
 		var mysql = req.app.get('mysql');
-		var sql = "INSERT INTO artist (name, hometown, birthday, deathday, biography) VALUES  (?, ?, ?, ?, ?);"
-//INSERT INTO artwork (title, artist_id, category, date, thumbnail_url, partner_id) VALUES (?, (SELECT id FROM artist WHERE name = ? and birthday=?), ?, ?, ?, ?);\
-//INSERT INTO artwork_gene (artwork_id, gene_id) VALUES ((SELECT id FROM artwork where title=? and artist_id = (SELECT id FROM artist WHERE name = ? and birthday=?) and date = ?), ?);";
-		var inserts = [req.body.name, req.body.hometown, req.body.birthday, req.body.deathday, req.body.biography];//, 
-				///req.body.artwork_title, req.body.name, req.body.birthday, req.body.artwork_category, req.body.artwork_date, req.body.artwork_thumbnail,req.body.artwork_partner,
-				//req.body.artwork_title, req.body.name, req.body.birthday, req.body.artwork_date, parseInt(req.body.genes_to_link)];
+		var sql = "INSERT INTO artist (name, hometown, birthday, deathday, biography) VALUES  (?, ?, ?, ?, ?);";
+		var inserts = [req.body.name, req.body.hometown, req.body.birthday, req.body.deathday, req.body.biography];
 		sql = mysql.pool.query(sql, inserts, function(error, results, fields){
 			if (error) {
 				res.write(JSON.stringify(error));
 				res.end;
 			} else
 			{
-				res.redirect('/artist');
+				sql = "INSERT INTO artwork (title, artist_id, category, date, thumbnail_url, partner_id) VALUES (?, (SELECT id FROM artist WHERE name = ? and birthday=?), ?, ?, ?, ?);";
+				inserts = [req.body.artwork_title, req.body.name, req.body.birthday, req.body.artwork_category, req.body.artwork_date, req.body.artwork_thumbnail,req.body.artwork_partner];
+				sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+					if(error) {
+						res.write(JSON.stringify(error));
+						res.end;
+					}
+					else {
+					sql = "INSERT INTO artwork_gene (artwork_id, gene_id) VALUES ((SELECT id FROM artwork where title=? and artist_id = (SELECT id FROM artist WHERE name = ? and birthday=?) and date = ?), ?);";
+					inserts = [req.body.artwork_title, req.body.name, req.body.birthday, req.body.artwork_date, parseInt(req.body.genes_to_link)];
+					sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+						if(error) {
+							res.write(JSON.stringify(error));
+							res.end;
+						}
+						else {
+							res.redirect('/artist');
+						}
+					});
+					}
+				});			
 			}
-		});			
+		});
 	});
 
 
