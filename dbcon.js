58a5cc6362efd082ad9796f36f3c04f7e8fc5099
Christina Brasco <christina.brasco@gmail.com>
2019-05-23 23:49:53 -0700

    fixing typo

--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : process.env.JAWS_HOST_VAL,
-  user            : process.env.JAWS_USER_process.env.,
-  password        : process.env.JAWS_PW_process.env.,
-  database        : process.env.JAWS_DB_process.env.
+  user            : process.env.JAWS_USER_VAL,
+  password        : process.env.JAWS_PW_VAL,
+  database        : process.env.JAWS_DB_VAL
 });
 module.exports.pool = pool;
