eb79818b0e9d37bed8bd87000b712147a0aa1320
Christina Brasco <christina.brasco@gmail.com>
2019-05-27 21:29:06 -0700

    testing for null text URL

--- public/cleanURL.js
+++ public/cleanURL.js
@@ -1,6 +1,6 @@
 function cleanPhotoURL(photoURL)
 {
-	if (photoURL)
+	if (photoURL && photoURL!="null")
 		return photoURL;
 	else 
 	    return "https://upload.wikimedia.org/wikipedia/commons/b/b1/Missing-image-232x150.png";
