52ea91b54825be5749937b56e42848a43958f5bd
Christina Brasco <christina.brasco@gmail.com>
2019-05-27 20:58:15 -0700

    enclosing in double quotes

--- public/cleanURL.js
+++ public/cleanURL.js
@@ -1,10 +1,15 @@
 function cleanPhotoURL(photoURL)
 {
-	var http = new XMLHttpRequest();
-    http.open('HEAD', image_url, false);
-    http.send();
-    if (http.status <400)
-    	return photoURL;
-    else 
-    	return "https://upload.wikimedia.org/wikipedia/commons/b/b1/Missing-image-232x150.png";
+	if (photoURL)
+	{
+		var http = new XMLHttpRequest();
+	    http.open('HEAD', image_url, false);
+	    http.send();
+	    if (http.status <400)
+	    	return photoURL;
+	    else 
+	    	return "https://upload.wikimedia.org/wikipedia/commons/b/b1/Missing-image-232x150.png";
+	}
+	else 
+	    return "https://upload.wikimedia.org/wikipedia/commons/b/b1/Missing-image-232x150.png";
 }
