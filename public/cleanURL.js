e8dae4599c0668a16cecb7a0a1161e994621c22b
Christina Brasco <christina.brasco@gmail.com>
2019-06-01 18:42:24 -0700

    adding debug text for cleanURL function

--- public/cleanURL.js
+++ public/cleanURL.js
@@ -3,5 +3,8 @@ function cleanPhotoURL(photoURL)
 	if (photoURL && photoURL!="null")
 		return photoURL;
 	else 
+	{
+		console.log("filling with missing URL");
 	    return "https://upload.wikimedia.org/wikipedia/commons/b/b1/Missing-image-232x150.png";
+	}
 }
